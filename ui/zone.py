b32f3fb7d6c084d7d034d7b5b6d43d391f070439
breathingstatue <149799034+breathingstatue@users.noreply.github.com>
2024-03-12 19:21:06 +0200

    fixed track zones and some ui elements

--- ui/zone.py
+++ ui/zone.py
@@ -6,19 +6,19 @@ class RVIO_PT_RevoltZonePanel(bpy.types.Panel):
     bl_space_type = "PROPERTIES"
     bl_region_type = "WINDOW"
     bl_context = "scene"
-
-    def draw_header(self, context):
-        self.layout.label(text="Track Zones")
+    bl_options = {"HIDE_HEADER"}
 
     def draw(self, context):
-        view = context.space_data
-        obj = context.object
-        props = context.scene.revolt
         layout = self.layout
-        layout.operator("scene.add_track_zone", icon="MATCUBE", text="Create Track Zone")
-        layout.operator("scene.zone_hide", icon="RESTRICT_VIEW_ON")
+        
+        box = layout.box()
+        box.label(text="Track Zones:")
+        col = box.column(align=True)
+        col.operator("scene.add_track_zone", icon="MATCUBE", text="Create Track Zone")
+        col.operator("scene.zone_hide", icon="RESTRICT_VIEW_ON")
              
         # Hull properties
+        box = layout.box()
         box.label(text="Hull Properties:")
         col = box.column(align=True)
         col.operator("scene.add_hull_sphere")
