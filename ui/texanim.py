6a72754b0b10b4ee26e8ba1a3a03c4881012e5d2
breathingstatue <149799034+breathingstatue@users.noreply.github.com>
2024-03-13 05:12:06 +0200

    worked on Texture Animations

--- ui/texanim.py
+++ ui/texanim.py
@@ -1,5 +1,6 @@
 import bpy
 import bmesh
+from ..operators import OBJECT_OT_add_texanim_uv, RVIO_OT_TexAnimTransform
 
 class RVIO_PT_AnimModesPanel(bpy.types.Panel):
     bl_idname = "RVIO_PT_AnimModesPanel"
@@ -7,10 +8,13 @@ class RVIO_PT_AnimModesPanel(bpy.types.Panel):
     bl_space_type = "PROPERTIES"
     bl_region_type = "WINDOW"
     bl_context = "object"
-
+    
     def draw(self, context):
         layout = self.layout
+        scene = context.scene
 
-        layout.operator("texanim.transform", icon="ARROW_LEFTRIGHT")
-        layout.operator("texanim.grid", icon="MESH_GRID")
-
+        layout.prop(scene, 'ta_max_frames', slider=True)
+        layout.prop(scene, 'ta_max_slots', slider=True)
+        layout.prop(scene, 'frame_duration', slider=True)
+        layout.operator("object.add_texanim_uv", text="Add Animation UV Layer")
+        layout.operator("rvio.texanim_transform", text="Send UV AnimLayers for Export")
