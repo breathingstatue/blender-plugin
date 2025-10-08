de51cece65bd6a7338e11a2890b4e23d48a78d36
breathingstatue <149799034+breathingstatue@users.noreply.github.com>
2025-10-08 09:31:01 +0300

    updated version number

--- __init__.py
+++ __init__.py
@@ -110,7 +110,7 @@ from .ui.migpanel import RVIO_PT_RevoltMIGPanel
 bl_info = {
 "name": "Re-Volt",
 "author": "Marvin Thiel & Theman",
-"version": (20, 25, 60),
+"version": (20, 25, 70),
 "blender": (4, 5, 1),
 "location": "File > Import-Export",
 "description": "Import and export Re-Volt file formats.",
